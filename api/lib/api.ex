a37b6cf365d5c8d37051babd30397060abec5008
Sheena Artrip <sheena.artrip@gmail.com>
2014-09-28 20:42:49 -0400

    Update api.ex

--- api/lib/api.ex
+++ api/lib/api.ex
@@ -2,7 +2,7 @@ defmodule Api do
   use Application
 
   def start(_a,_b) do
-    IO.puts "Running MyPlug with Cowboy on http://localhost:4000"
+    IO.puts "Running API Endpoint with Cowboy on http://localhost:4000"
     Plug.Adapters.Cowboy.http Endpoint, []
     {:ok, self()}
   end
